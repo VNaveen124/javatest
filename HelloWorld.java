261a8245e7507fb35c8feed44583c923a4ceb7b2
Ubuntu <ubuntu@ip-172-31-11-233.ap-south-1.compute.internal>
2025-04-14 08:46:25 +0000

    java code modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+public class HelloTeam {
     public static void main(String[] args) {
-        System.out.println("Hello, World from Docker!");
+        System.out.println("Hello Team, This is Naveen Kumar, How may I Help you");
     }
 }
